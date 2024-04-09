2b5af63a146d7e397e32b4e18a26b7806123f727
lcflight <lcarthur747@gmail.com>
2024-04-09 09:07:30 -0400

    converted to cli

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, world!");
+#!/usr/bin/env node
+
+console.log("Hello, from index!");
+
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+void yargs(hideBin(process.argv)).command(
+  "hello",
+  "Prints a greeting",
+  {},
+  () => {
+    console.log("Hello, world!");
+  }
+);
